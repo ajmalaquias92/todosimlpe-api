f075a2acde816b06c6daed82259629976b0d8d52
Anselmo João Malaquia <anselmo1992malaquia@gmail.comgit config --global user.email anselmo1992malaquia@gmail.comgit config --global user.email anselmo1992malaquia@gmail.com>
2026-09-17 05:37:52 +0100

    Criação do modelo de usuário, inserção de extensões recomendadas e adição da dependência de validação do Spring Boot

---
+++ src/main/java/com/adrosystems/todosimple/models/User.java
@@ -0,0 +1,109 @@
+package com.adrosystems.todosimple.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+@Entity
+@Table(name = User.TABLE_NAME)
+public class User {
+    public static final String TABLE_NAME = "users";
+
+    public interface CreateUser {
+    }
+
+    public interface UpdateUser {
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
+    private Long id;
+
+    @Column(name = "user_name", length = 100, nullable = false, unique = true)
+    @NotNull(groups = CreateUser.class)
+    @NotEmpty(groups = CreateUser.class)
+    @Size(groups = CreateUser.class, min = 3, max = 100)
+    private String username;
+
+    @JsonProperty(access = Access.WRITE_ONLY)
+    @Column(name = "password", length = 60, nullable = false)
+    @NotNull(groups = { CreateUser.class, UpdateUser.class })
+    @NotEmpty(groups = { CreateUser.class, UpdateUser.class })
+    @Size(groups = { CreateUser.class, UpdateUser.class }, min = 8, max = 60)
+    private String password;
+
+    //private List<Task> tasks = new ArrayList<Task>();
+
+    public User() {
+    }
+
+
+    public User(Long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override 
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        if (!(o instanceof User)) return false;
+        
+        User other = (User) o;
+        if (this.id == null) {
+            if (other.id != null) return false;
+        } else if (!(this.id.equals(other.id))) {
+            return false;
+        }
+
+        return Objects.equals(id, other.id) && Objects.equals(username, other.username) && Objects.equals(password, other.password); 
+    }
+
+    @Override 
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (this.id == null ? 0 : this.id.hashCode());
+        return result;
+    }
+}

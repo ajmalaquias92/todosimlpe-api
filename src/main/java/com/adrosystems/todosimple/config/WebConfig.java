c3db09cc29bd1a9a365ffd6beb0ddc85e01119b7
Anselmo João Malaquia <anselmo1992malaquia@gmail.comgit config --global user.email anselmo1992malaquia@gmail.comgit config --global user.email anselmo1992malaquia@gmail.com>
2026-09-19 20:50:02 +0100

    Comicação do back-end com o front-end

--- src/main/java/com/adrosystems/todosimple/config/WebConfig.java
+++ src/main/java/com/adrosystems/todosimple/config/WebConfig.java
@@ -10,6 +10,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/''");
+        registry.addMapping("/**").allowedOrigins("http://127.0.0.1:5500");
     }    
 }
